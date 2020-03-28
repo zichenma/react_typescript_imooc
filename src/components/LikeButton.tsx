3eaeedcef54752c6e0cd3b31747234c619c75c84
zichenma <zma@liaisionedu.com>
2020-03-28 16:58:57 -0400

    finished 3_6

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const LikeButton: React.FC = () => {
     const [like, setLike] = useState(0);
     const [on, setOn] = useState(true);
+    useEffect(() => {
+        document.title = `clicked ${like}`;
+    })
     return (
         <>
         <button onClick={() => {setLike(like + 1)}}>
